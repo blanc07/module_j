0c22dabb8f2954aea37c0ec3690a749dd0322181
blanc <muamarmaga@gmail.com>
2023-07-28 20:05:24 +0700

    Урок 7, задание 4. Новые: имя модели, вывод объектов.

--- mysweetsite/app_advertisements/models.py
+++ mysweetsite/app_advertisements/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField('заголовок', max_length=60)
+    description = models.TextField('описание')
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+    is_auction = models.BooleanField('уместен ли торг', help_text='Отметьте, если торг по объявлению уместен.')
+    updated_at = models.DateTimeField('дата обновления', auto_now=True)
+    created_at = models.DateTimeField('дата публикации', auto_now_add=True)
+
+# новый вывод
+    def __str__(self):
+        return f'Advertisement(id={self.pk}, title={self.title}, price={self.price})'
+
+# новое имя
+    class Meta:
+        db_table = 'advertisements'
