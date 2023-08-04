8c6d2a7418f854a81162b5b363bb112304713a90
blanc <muamarmaga@gmail.com>
2023-08-04 20:51:25 +0700

    Урок 8, задание 4

--- mysweetsite/app_advertisements/models.py
+++ mysweetsite/app_advertisements/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.utils.html import format_html
+from django.contrib import admin
+
+
 
 # Create your models here.
 
@@ -9,6 +13,26 @@ class Advertisement(models.Model):
     is_auction = models.BooleanField('уместен ли торг', help_text='Отметьте, если торг по объявлению уместен.')
     updated_at = models.DateTimeField('дата обновления', auto_now=True)
     created_at = models.DateTimeField('дата публикации', auto_now_add=True)
+
+    @admin.display(description='Дата публикации')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                "<span style='color: green; font-weight: bold'>Сегодня в {}<span/>", created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y - %H:%M')
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html(
+                "<span style='color: blue; font-weight: bold'>Сегодня в {}<span/>", updated_time
+            )
+        return self.updated_at.strftime('%d.%m.%Y - %H:%M')
 
 # новый вывод
     def __str__(self):
